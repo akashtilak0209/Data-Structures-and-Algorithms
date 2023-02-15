4493a93921c91f5c52d0fb2534dcde59181dfbf3
akash02090 <akash.tilak2900@gmail.com>
2023-02-15 23:12:53 +0530

    Todays DSA

---
+++ 15022023/daily.py
@@ -0,0 +1,35 @@
+# Question: https://leetcode.com/problems/add-to-array-form-of-integer/
+
+# My Solution:(Brute Force)
+def mySolution(num,k):
+    integer = 0
+    n = len(num)
+    for i,el in enumerate(num):
+        integer = integer + el*(10**(n-i-1))
+    #integer + k
+    integer += k
+    #integer to list
+    lis=[]
+    for element in str(integer):
+        lis.append(element)
+
+    return list(map(int,lis))
+
+# Optimized Solution:
+
+# Original Solution:
+def originalSolution(num,k):
+    num[-1] += k
+    for i in range(len(num) - 1, -1, -1):
+        carry, num[i] = divmod(num[i], 10)
+        if i: num[i-1] += carry
+    if carry:
+        num = map(int, str(carry)) + num
+    return num
+
+
+if __name__ == '__main__':
+    num = [1,2,0,0]
+    k = 34
+    print(mySolution(num, k))
+    print(originalSolution(num, k))
